3351edcf9be07fade778ed93c9d2b31e098c53ae
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-09-09 04:19:34 +0300

    Done

---
+++ src/js/helpers.js
@@ -0,0 +1,61 @@
+const debounce = (fn, debounceTime) => {
+  let timer;
+
+  return function() {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, debounceTime);
+  };
+};
+
+function createRepoItem (item) {
+  let newItem = document.createElement('div');
+  newItem.className = 'repositories__item';
+
+  const span = document.createElement('span');
+  span.className = 'repositories__item--span';
+  const name = span.cloneNode(true);
+  const owner = span.cloneNode(true);
+  const stars = span.cloneNode(true);
+  name.textContent = `Name: ${item.name}`;
+  owner.textContent = `Owner: ${item.owner.login}`;
+  stars.textContent = `Stars: ${item.stargazers_count}`;
+
+  const btn = document.createElement('button');
+  btn.className = 'repositories__deleteButton';
+  btn.id = 'repoDelete';
+  newItem.append(name, owner, stars, btn);
+  repositories.append(newItem);
+
+  githubSearch.value = '';
+}
+
+function createSearchItem (item, id) {
+  let newItem = document.createElement('li');
+  newItem.className = 'results__item';
+  newItem.textContent = item;
+  newItem.id = 'item' + id;
+
+  results.append(newItem);
+}
+
+function clearSearchItems () {
+  results.innerHTML = '';
+}
+
+async function getRepositories (req) {
+  const url = new URL('https://api.github.com/search/repositories');
+  url.search = 'q=' + req;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log('nothing found');
+    return;
+  } else if (response.status !== 200) {
+    console.log('Error, status code ' + response.status);
+  }
+  const items = await response.json();
+  return items.items;
+}
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+import '../scss/style.scss';
+
+// Remove added repo buttons
+repositories.addEventListener('click', (e) => {
+  if (e.target.classList.contains('repositories__deleteButton')) {
+    e.target.closest('.repositories__item').remove();
+  }
+});
+
+// Adding repos to list
+results.addEventListener('click', (e) => {
+  if (e.target.classList.contains('results__item')) {
+    const item = sessionStorage.getItem(e.target.id);
+    createRepoItem(JSON.parse(item));
+    clearSearchItems();
+  }
+});
+
+// Searching
+async function search (event) {
+  const searchString = await getRepositories(event.target.value);
+  clearSearchItems();
+  for (let i = 0; i < 5; i++) {
+    sessionStorage.setItem('item' + i, JSON.stringify(searchString[i]));
+    createSearchItem(searchString[i].name, i);
+  }
+}
+
+const searchDebounced = debounce(search, 300);
+githubSearch.addEventListener('keyup', searchDebounced);
